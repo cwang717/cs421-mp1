e6ad301d97df9ad6076ecd47d8b8ffd7069ad24d
laplacecrame <laplacecrame@gmail.com>
2025-06-01 19:27:30 -0700

    ADT

--- mp1b-adts/src/Lib.hs
+++ mp1b-adts/src/Lib.hs
@@ -25,37 +25,57 @@ data Exp = IntExp Integer
 --- ### list2cons
 
 -- don't forget to put the type declaration or you will lose points!
-list2cons = undefined
+list2cons :: [a] -> List a
+list2cons [] = Nil
+list2cons (x:xs) = Cons x (list2cons xs)
 
 --- ### cons2list
 
 -- don't forget to put the type declaration or you will lose points!
-cons2list = undefined
+cons2list :: List a -> [a]
+cons2list Nil = []
+cons2list (Cons x consxs) = x:(cons2list consxs)
 
 --- ### eval
 
 -- don't forget to put the type declaration or you will lose points!
-eval = undefined
+eval :: Exp -> Integer
+eval (IntExp x) = x
+eval (PlusExp es) = foldr (+) 0 (map eval es)
+eval (MultExp es) = foldr (*) 1 (map eval es)
 
 --- ### list2cons'
 
 -- don't forget to put the type declaration or you will lose points!
-list2cons' = undefined
+list2cons' :: [a] -> List a
+list2cons' = foldr (\x consxs -> Cons x consxs) Nil
 
 --- ### BinTree
 
 -- BinTree
+data BinTree a = Node a (BinTree a) (BinTree a)
+               | Leaf
+  deriving (Show)
 
 --- ### sumTree
 
 -- don't forget to put the type declaration or you will lose points!
-sumTree = undefined
+sumTree :: Num a => BinTree a -> a
+sumTree Leaf = 0
+sumTree (Node x lc rc) = x + (sumTree lc) + (sumTree rc)
 
 --- ### SimpVal
 
 -- SimpVal
+data SimpVal = IntVal Integer
+             | BoolVal Bool
+             | StrVal String
+             | ExnVal String
+  deriving (Show)
 
 --- ### liftIntOp
 
 -- don't forget to put the type declaration or you will lose points!
-liftIntOp = undefined
+liftIntOp :: (Integer -> Integer -> Integer) -> SimpVal -> SimpVal -> SimpVal
+liftIntOp op (IntVal a) (IntVal b) = IntVal (op a b)
+liftIntOp op _ _ = ExnVal "not an IntVal!"
